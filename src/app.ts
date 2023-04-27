a797842c1788066c8640038eb241f13a226e6131
Victor Hugo <victorhdoliveira@gmail.com>
2023-04-27 15:27:38 -0300

    Feat: Endpoint POST /motorcyle

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
 import carsRoute from './Routes/carsRoute';
+import motorcycleRoute from './Routes/motorcycleRoute';
 
 const app = express();
 app.use(express.json());
 app.use(carsRoute);
+app.use(motorcycleRoute);
 app.use(ErrorHandler.handle);
 
 export default app;
